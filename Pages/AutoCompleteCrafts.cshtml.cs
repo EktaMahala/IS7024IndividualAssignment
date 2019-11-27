09fb845cbcd3868907ac2c7339804eb6a08ac233
EktaMahala <54607380+EktaMahala@users.noreply.github.com>
2019-11-27 11:02:07 -0500

    Initial commit

---
+++ Pages/AutoCompleteCrafts.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IS7024IndividualAssignment.Pages
+{
+    public class AutoCompleteCraftsModel : PageModel
+    {
+        [BindProperty]
+        private string Term { get; set; }
+        private IList<string> craftNames = new List<string>();
+        public JsonResult OnGet()
+        {
+            Term = HttpContext.Request.Query["term"];
+
+            AddCrafts("Quiling");
+            AddCrafts("Paper Cutting");
+            AddCrafts("Paper Folding");
+            AddCrafts("Paper Model");
+            AddCrafts("Painting");
+            AddCrafts("Paper Layering");
+            AddCrafts("Crafting flowers");
+            AddCrafts("Crafting Cards");
+            AddCrafts("Quiling  Card");
+            
+
+            return new JsonResult(craftNames);
+        }
+
+        private void AddCrafts(string craftName)
+        {
+            string lowername = craftName.ToLower();
+            string lowerterm = Term.ToLower();
+            if (lowername.Contains(lowerterm))
+            {
+                craftNames.Add(craftName);
+            }
+        }
+    }
+}
